9416e114ceeaa3487c84efaf526dec23ced7ce1e
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2016-03-16 19:28:41 +0000

    green: say Fizz

--- test/xxxShould.js
+++ test/xxxShould.js
@@ -2,14 +2,32 @@
 let should = require('chai').should();
 describe('OCP Kata', () => {
   it('say the number', () => {
-    ocpkata().say(1).should.equal('1');
-    ocpkata().say(2).should.equal('2');
+    ocpkata(DefaultRule()).say(1).should.equal('1');
+    ocpkata(DefaultRule()).say(2).should.equal('2');
+  });
+
+  it('say Fizz', () => {
+    ocpkata(FizzRule()).say(3).should.equal('Fizz');
+    ocpkata(FizzRule()).say(6).should.equal('Fizz');
   });
 
 })
 
-function ocpkata() {
+function ocpkata(rule) {
+
+  return {
+    say : (number) => rule.say(number)
+    }
+}
+
+function DefaultRule() {
   return {
     say: (number) => `${number}`
     }
+}
+
+function FizzRule() {
+  return {
+    say: (number) => 'Fizz'
+  }
 }
